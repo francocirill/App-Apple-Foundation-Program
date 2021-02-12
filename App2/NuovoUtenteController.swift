393d2b917fbde242733755eb3f0bee0550c75629
mar-ven <marcovenere99@outlook.com>
2021-02-12 13:26:53 +0100

    inizio creazione storyboard

---
+++ App2/NuovoUtenteController.swift
@@ -0,0 +1,35 @@
+//
+//  NuovoUtenteController.swift
+//  App2
+//
+//  Created by Marco Venere on 12/02/21.
+//
+
+import UIKit
+
+class NuovoUtenteController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var pictureSwitch: UISwitch!
+    @IBOutlet weak var speechSwitch: UISwitch!
+    @IBAction func saveButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
