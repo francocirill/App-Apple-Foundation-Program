bbdf2997e3b2cdc03dc12f06868a851daa531ef0
stefano-damato <s.damato.1999@gmail.com>
2021-04-07 18:27:19 +0200

    mondiviewconroller

---
+++ App2/App2/MondoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MondoViewController.swift
+//  App2
+//
+//  Created by Stefano D’Amato on 07/04/21.
+//
+
+import UIKit
+
+class MondoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [unowned self] in
+                    self.performSegue(withIdentifier: "start", sender: nil)
+                }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
