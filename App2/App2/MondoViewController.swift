b757eab07639c4111b7ca1568b97fe4a137b0a58
giusda99 <giusda99@users.noreply.github.com>
2021-04-10 13:21:05 +0200

    nuovi livelli e schermata mondi

--- App2/App2/MondoViewController.swift
+++ App2/App2/MondoViewController.swift
@@ -35,25 +35,28 @@ class MondoViewController: UIViewController {
             var user = PersistenceManager.fetchData()[0]
             switch user.lastLevel+1 {
             case  1..<11:
-                mondo.text="Mondo 1"
+                mondo.text="Bosco delle T"
                 background.image = UIImage(named: "mondo1.jpg")!
             case  11..<21:
-                mondo.text="Mondo 2"
+                mondo.text="Isola delle R"
                 background.image = UIImage(named: "mondo2.png")!
             case  21..<31:
-                mondo.text="Mondo 3"
+                mondo.text="Precipizio delle S"
                 background.image = UIImage(named: "mondo3.jpg")!
             case  31..<41:
-                mondo.text="Mondo 4"
+                mondo.text="Abisso delle GN"
                 background.image = UIImage(named: "mondo4.png")!
             case  41..<51:
-                mondo.text="Mondo 5"
+                mondo.text="Foresta delle SC"
                 background.image = UIImage(named: "mondo5.jpg")!
             default:
                 mondo.text="Other"
             }
             
         }
+        mondo.backgroundColor = UIColor(white: 1, alpha: 0.7)
+        mondo.layer.masksToBounds = true
+        mondo.layer.cornerRadius = 10
     }
     /*
     // MARK: - Navigation
