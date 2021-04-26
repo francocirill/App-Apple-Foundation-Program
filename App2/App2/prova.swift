459e46069df7dab0b8690f5b8d25d580d7401c61
stefano-damato <s.damato.1999@gmail.com>
2021-04-26 22:16:46 +0200

    prova

--- App2/App2/prova.swift
+++ App2/App2/prova.swift
@@ -23,12 +23,12 @@ class prova: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 14123
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 333
+        return 0
     }
 
     /*
